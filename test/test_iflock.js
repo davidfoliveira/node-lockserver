67953bd4233128f866bf69d215bffbf47f76df85
David Oliveira <d.oliveira@prozone.org>
2014-09-18 14:36:45 +0100

    Tests

--- test/test_iflock.js
+++ test/test_iflock.js
@@ -2,7 +2,7 @@ var
 	LockServer = require('../lib/lockserver').Client,
 	lockserver = new LockServer({host: "127.0.0.1", DEBUG: true});
 
-lockserver.lock("stuff",function(unlock){
+lockserver.lock(["stuff1","stuff2"],function(unlock){
 
         console.log("I am running");
         setTimeout(function(){
@@ -11,12 +11,12 @@ lockserver.lock("stuff",function(unlock){
         }, 3000);
 
 });
-lockserver.ifLock("stuff",
+lockserver.ifLock("stuff2",
 	function(){
 		console.log("IS LOCKED");
 	},
 	function(unlock){
-		console.log("I am running");
+		console.log("I am running!!");
 		unlock();
 		process.exit(0);
 	}
